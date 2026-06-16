6064f9238e131e29c858e5ad78af325464b199dc
Scriptguy69 <eneskucuk2012@gmail.com>
2026-06-16 21:09:34 +0200

    Add loadstring for Delta Executor

---
+++ loadstring_delta.lua
@@ -0,0 +1,5 @@
+-- Delta Executor Loadstring for Grow a Garden 2 Dupe Script
+-- Copy this entire line and paste into Delta Executor console:
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Scriptguy69/Robloc-scrip/main/Robloc%20Script.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Scriptguy69/Robloc-scrip/main/Robloc%20Script.lua"))()
